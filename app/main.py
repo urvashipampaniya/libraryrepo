3b91e519972f04d157e0a85ae3d744463bedf2dc
urvashipampaniya <167390319+urvashipampaniya@users.noreply.github.com>
2025-02-28 16:50:52 +0530

    update fils

--- app/main.py
+++ app/main.py
@@ -4,18 +4,23 @@ from app.config.database import create_all_tables
 from app.api.routers.books import router as books_router
 from app.api.routers.members import router as members_router
 from app.api.routers.loan import router as loans_router
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
    
     await create_all_tables()
     yield
-    print("Application is shutting down...")
+    logger.info("Application is shutting down...")
 
 app = FastAPI(title="Library Management System", lifespan=lifespan)
 
 
-app.include_router(books_router)
-app.include_router(members_router)
-app.include_router(loans_router)
+from app.api import api_router
+app.include_router(api_router)
+
 
